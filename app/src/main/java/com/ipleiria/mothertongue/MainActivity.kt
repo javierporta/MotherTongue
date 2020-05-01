391dd4444f71e2524e3ce682c5d8023d3fa6d685
Javier <2190271@my.ipleiria.pt>
2020-05-01 14:53:01 +0100

    Modify main layout

--- app/src/main/java/com/ipleiria/mothertongue/MainActivity.kt
+++ app/src/main/java/com/ipleiria/mothertongue/MainActivity.kt
@@ -6,16 +6,20 @@ import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.ipleiria.mothertongue.databinding.ActivityMainBinding
+import com.ipleiria.mothertongue.models.MainModel
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val mainModel: MainModel = MainModel("", "Park")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        //bind mainModel ToDo: Create language spinner adapter. Get current lanuage and current place
+        binding.mainModel = mainModel
 
     }
 
